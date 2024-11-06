0d44a889116f5a831d1c7a9f3d35a6c627da0f04
Ruby Ursus <rubyursus@gmail.com>
2024-11-06 13:11:36 +0200

    UndoMovement scipt added

---
+++ Assets/Scripts/PlayerController/UndoMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UndoMovement : MonoBehaviour
+{
+
+    Stack<Vector3> undoStack = new Stack<Vector3>();
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
