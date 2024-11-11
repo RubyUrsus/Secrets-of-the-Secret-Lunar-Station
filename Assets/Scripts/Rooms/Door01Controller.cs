45fa1f7bdee2e4cf4b613dd0e23ac2c8213867d1
Ruby Ursus <rubyursus@gmail.com>
2024-11-11 21:16:23 +0200

    Room 01 placeholder added

---
+++ Assets/Scripts/Rooms/Door01Controller.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door01Controller : MonoBehaviour
+{
+
+    public bool doorOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
