5c4a6a93c962e1b1d211b968385bd3b1ab23b90a
Ebe040 <emma.bertling@gmail.com>
2024-11-12 13:37:46 +0200

    Player following Enemy AI (prefab)

---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyHealth : MonoBehaviour, IDamageable
+{
+    Animator animator;
+    [SerializeField]
+    float enemyHealth;
+    NavMeshAgent agent;
+    public bool isDead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void KillEntity()
+    {
+        //GameManager.Instance.TrackEnemies();
+        animator.SetTrigger("Death");
+        isDead = true;
+        Debug.Log("Killed an enemy");
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        enemyHealth -= damageAmount;
+        Debug.Log("took damage" + enemyHealth);
+        if (enemyHealth == 0)
+        {
+            KillEntity();
+        }
+    }
+}
