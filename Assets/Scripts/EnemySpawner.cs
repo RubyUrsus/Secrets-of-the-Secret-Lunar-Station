ce0712aec3759c6cf4f5780b59700450408b803c
Ebe040 <emma.bertling@gmail.com>
2024-11-13 14:48:13 +0200

    welp (broken spawner) spawns all at once
    don't take unless you need it for testing

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    float SpawnTime;
+    [SerializeField]
+    private float timer;
+    [SerializeField]
+    int maxEnemiesToSpawn;
+
+    [SerializeField]
+    GameObject enemy;
+
+    private void Start()
+    {
+        //if ((GameObject == null) = FindObjectOfType<GameObject>();
+    }
+
+    public void Update()
+    {
+        
+        timer += Time.deltaTime;
+        if (timer >= SpawnTime)
+        {
+            Debug.Log("Spawning started");
+
+            while (maxEnemiesToSpawn > 0)
+            {
+                Debug.Log(maxEnemiesToSpawn - 1);
+                maxEnemiesToSpawn = maxEnemiesToSpawn - 1;
+                SpawnEnemies();
+                timer = 0;
+            }
+        }
+    }
+
+    private void SpawnEnemies()
+    {
+        Instantiate(enemy, transform.position, Quaternion.identity);
+        timer = 0;
+        
+    }
+}
